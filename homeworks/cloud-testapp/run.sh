48834ee9bac1db87b9637511f903698f552af6ae
drassadnikov <drassadnikov@diasoft.ru>
2022-09-30 04:58:29 +0300

    Update run.sh

--- homeworks/cloud-testapp/run.sh
+++ homeworks/cloud-testapp/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 EXIT_STATUS=0
 
+chmod +x ./install_ruby.sh
+chmod +x ./install_mongodb.sh
+chmod +x ./deploy.sh
 ./install_ruby.sh
 ./install_mongodb.sh
 ./deploy.sh
 inspec exec otus-homeworks/homeworks/$BRANCH || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
-
-
-
